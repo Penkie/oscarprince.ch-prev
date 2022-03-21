495c79f47001c4602185b943fd9be8c861261b59
Oscar <heatblack09@gmail.com>
2022-03-21 13:59:18 +0000

    feat: some style changes and home content

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  public colors = ["#e24444", "#e28344", "#71e244", "#44d7e2", "#446ee2", "#8e44e2", "#e24486"];
+  public selectedColor: string = '';
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.selectedColor = this.selectNewColor();
+  }
+
+  public selectNewColor(): string {
+    return this.colors[Math.floor(Math.random() * this.colors.length )];
   }
 
 }
